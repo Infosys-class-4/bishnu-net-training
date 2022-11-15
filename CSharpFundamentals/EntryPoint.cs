dd5da41b374f73f1cd4569b6072abbbfb2ea56fc
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-11-15 12:21:28 +0545

    Learn about if else and switch

--- CSharpFundamentals/EntryPoint.cs
+++ CSharpFundamentals/EntryPoint.cs
@@ -22,14 +22,80 @@ else
 // Classwork: Ask user to enter his name and convert that to uppercase and 
 // print on console.
 
+int mark = 45;
 
-// Creating objects
-// Initializing fields
-// Person person1 = new();
-// person1.name = "Sitaram";
-// person1.age = 12;
+// If marks is less than 40, print "You failed"
+// If marks is less than 60 and greater than 40, print "You Passed with second division"
+// If marks is less than 80 and greater than 60, print "You Passed with first division"
+// If marks is greater than 80, print "You Passed with distiction"
 
-// string message = person1.GetLegalGreeting();
-// Console.WriteLine(message);
+if (mark < 40)
+{
+    Console.WriteLine("You failed");
+}
+else if (mark < 60)
+{
+    Console.WriteLine("You Passed with second division");
+}
+else if (mark < 80)
+{
+    Console.WriteLine("You Passed with first division");
+}
+else
+{
+    Console.WriteLine("You Passed with distiction");
+}
 
+// Print corresponding roman equivalents for numbers upto 5
+// 1 => I
+// 2 => II
+// 3 => III
+// 4 => IV
+// 5 => V
+
+var z = Console.ReadLine();
+var n = int.Parse(z);
+var result = "";
+
+if (n is 1)
+{
+    result = "I";
+}
+else if (n is 2)
+{
+    result = "II";
+}
+else if (n is 3)
+{
+    result = "III";
+}
+else if (n is 4)
+{
+    result = "IV";
+}
+else if (n is 5)
+{
+    result = "V";
+}
+
+Console.WriteLine(result);
+
+// Switch
+switch(n)
+{
+    case 1: result = "I";
+    break;
+    case 2: result = "II";
+    break;
+    case 3: result = "III";
+    break;
+    case 4: result = "IV";
+    break;
+    case 5: result = "V";
+    break;
+}
+
+Console.WriteLine(result);
+
+// Loops or iteration statements
 
