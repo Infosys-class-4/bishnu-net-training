845555aecbb14e880765d628b435049c9c56f3e6
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-12-14 10:13:38 +0545

    Solve assignment

---
+++ WebDevelopment/HelloWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWeb.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string[] employeeNames = { "Bishnu Rawal", "Sangyel Tenzing", "Sakshyam Shrestha", "SitaRam Panta" };
+
+            return View(employeeNames);
+        }
+
+        //Assignment: Add new action here to render list of employee name and his/her phone number
+        // Hint: use disctionary data structure to hold all employees info
+
+    }
+}
