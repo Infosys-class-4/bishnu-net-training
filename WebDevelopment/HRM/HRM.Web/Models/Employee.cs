bcec8b73a3c7e834c063b947890299731a224cc0
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-12-16 10:04:04 +0545

    Created new HRM web template

---
+++ WebDevelopment/HRM/HRM.Web/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace HRM.Models;
+public class Employee
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public char Gender { get; set; } = 'M';
+    public string Address { get; set; } = string.Empty;
+    public DateTime Dob { get; set; }
+    public string Designation { get; set; } = string.Empty;
+    public string Department { get; set; } = string.Empty;
+}
